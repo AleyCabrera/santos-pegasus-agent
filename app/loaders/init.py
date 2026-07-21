af52d6b17ddff6bc574e8c773d8c1f998bf09016
Aley Cabrera D <cabreratrader.fxoficry@gmail.com>
2026-07-21 15:43:46 -0500

    feat: refactorizacion del proyecto para mejorar el proyecto

--- app/loaders/init.py
+++
@@ -1,7 +0,0 @@
-"""Módulo de cargadores de documentos"""
-
-from app.loaders.pdf_loader import PDFLoader
-from app.loaders.csv_loader import CSVLoader
-from app.loaders.base_loader import BaseLoader
-
-__all__ = ['PDFLoader', 'CSVLoader', 'BaseLoader']
